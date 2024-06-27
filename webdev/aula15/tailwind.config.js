7ace99f374082dc8253b3db20debc4bef5256a55
caldeira04 <joao.p.caldeira@icloud.com>
2024-06-27 11:35:02 -0300

    kkkkkkk

---
+++ webdev/aula15/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'second-gray': '#737373',
+        'gray': '#252B42',
+      },
+    },
+  },
+  plugins: [],
+}
